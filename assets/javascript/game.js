bcf27270d5adfaa37a9e7c542366766a89449d52
Jennifer Rubin <jenlrubin9326@gmail.com>
2018-10-22 14:02:07 -0400

    new js

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,88 +1,120 @@
+let gameHasStarted = false;
 
-$(document).ready(function(){
-    $("#hanSolo").on("click", function() {
-        $(".villanContainer").removeClass()
-       $("#lea").toggle()
-       $("#obi").toggle()
-       $("#r2").toggle()
-    })
+ let playerStats = {
+    hansolo : {
+        health: 180,
+        attack:7,
+        enemyAttackBack:25
+    },
+    r2 : {
+        health:150,
+        attack:8,
+        enemyAttackBack:20
+   
+    },
+    obiWan : {
+        health: 100,
+        attack:14,
+        enemyAttackBack:15
+    },
+    princessLea : {
+        health: 120,
+        attack:8,
+        enemyAttackBack : 15
+    },
+ }
+
+
+
+ 
+ var gameStarted = false;
+ var villanSelected = false; 
+ var playerSelected = false; 
+var myCharacter = "";
+var myDefender = ""
+var character; 
+var player; 
+var defeated = false
+var r2
+var obi
+var lea
+var hanSolo
+var attack;
+var health
+var enemyAttack; 
+
+
+// determines which player moves into my character and which move into fight
+$("#hanSolo").on("click", function() {
+    if (myCharacter === "" && playerSelected === false) {
+        $(this).appendTo(".myChar");
+        $("#lea").appendTo("#fight")
+        $("#obi").appendTo("#fight");
+        $("#r2").appendTo("#fight")
+        playerSelected = true; 
+        myChracter = playerStats.hanSolo
+    }
+    if (myCharacter === playerStats.hansolo)
+        attack = playerStats.hansolo.attack
+        health = playerStats.hansolo.health
+        enemyAttack = playerStats.hansolo.enemyAttackBack
 })
 
-$(document).ready(function(){
-    $("#lea").on("click", function() {
-        $(".villanContainer").removeClass()
-       $("#hanSolo").toggle()
-       $("#obi").toggle()
-       $("#r2").toggle()
-    })
+$("#lea").on("click", function() {
+    if (myCharacter === "" && playerSelected === false) {
+        $(this).appendTo(".myChar");
+        $("#hanSolo").appendTo("#fight")
+        $("#obi").appendTo("#fight");
+        $("#r2").appendTo("#fight")
+        playerSelected = true;
+        myCharacter = playerStats.princessLea
+    }if (myCharacter == playerStats.princessLea)
+    attack = playerStats.princessLea.attack
+    health = playerStats.princessLea.health
+    enemyAttack = playerStats.princessLea.enemyAttackBack
 })
 
-$(document).ready(function(){
-    $("#obi").on("click", function() {
-        $(".villanContainer").removeClass()
-       $("#lea").toggle()
-       $("#hanSolo").toggle()
-       $("#r2").toggle() 
-    })
+$("#obi").on("click", function() {
+    if (myCharacter === "" && playerSelected === false) {
+        $(this).appendTo(".myChar");
+        $("#lea").appendTo("#fight")
+        $("#hanSolo").appendTo("#fight");
+        $("#r2").appendTo("#fight")
+        playerSelected = true; 
+        myCharacter = playerStats.obiWan
+    }
+    if (myCharacter === playerStats.obiWan)
+        attack = playerStats.obiWan.attack
+        health = playerStats.obiWan.health
+        enemyAttack = playerStats.obiWan.enemyAttackBack
 })
 
-$(document).ready(function(){
-    $("#r2").on("click", function() {
-        $(".villanContainer").removeClass()
-       $("#lea").toggle()
-       $("#obi").toggle()
-       $("#hanSolo").toggle()
-    })
+$("#r2").on("click", function() {
+    if (myCharacter === "" && playerSelected === false) {
+        $(this).appendTo(".myChar");
+        $("#lea").appendTo("#fight")
+        $("#obi").appendTo("#fight");
+        $("#hanSolo").appendTo("#fight")
+        playerSelected = true; 
+        console.log(playerSelected)
+        myCharcter = playerStats.r2
+    }
+    if (myCharacter == playerStats.r2)
+        attack = playerStats.r2.attack
+        health = playerStats.r2.health
+        enemyAttack = playerStats.r2.enemyAttackBack
+    
 })
 
+//move characters from enemies to defender 
 
-$("#darth").on("click", function() {
-    var darth = $("#darth")
-    $("#defend").append(darth)
-})
-
-var gameHasStarted = false;
-
-
-var princessLea = {
-    health: 120,
-    attack:8,
-    enemyAttackBack : 15
+if (playerSelected == true) {
+    $("#hanSolo").on("click", function(){
+        myDefender = $(this)
+        $(myDefender).appendTo("#Defender")
+ })
 }
 
- var obiWan = {
-     health: 100,
-     attack:14,
-     enemyAttackBack:15
- }
 
- var r2 = {
-     health:150,
-     attack:8,
-     enemyAttackBack:20
 
- }
 
- var hansolo = {
-     health: 180,
-     attack:7,
-     enemyAttackBack:25
- }
-
- var darth = {
-     health:100, 
- }
-
- $("button").on("click",function(){
-     startGame()
- })
-
- function startGame() {
-    /// your character attacks defender 
-    playerAttack = function () {
-
-    }
-    // health is subtracted for both character and defender 
-    // on each attack press attack adds character +10
-    // 
- }                                    
